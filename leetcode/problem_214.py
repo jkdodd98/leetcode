172dd6e9ee8b5bd0c8b03e7ace274c0cfc1705a3
jake-olvin <jake@olvin.com>
2024-09-20 16:21:26 +0100

    refactor docstring

--- leetcode/problem_214.py
+++ leetcode/problem_214.py
@@ -1,30 +1,28 @@
 """
 214. Shortest Palindrome
+Hard
+Topics: `String`, `Rolling Hash`, `String Matching`, `Hash Function`
 
-- Difficulty: Hard
-- Acceptance Rate: 36.4%
-
-Topis: string, rolling hash, string matching, hash function
-
-You are given a string s. You can convert s to a palindrome by adding characters in
+You are given a string `s`. You can convert `s` to a palindrome by adding characters in
 front of it.
 
 Return the shortest palindrome you can find by performing this transformation.
 
 Example 1:
 
-Input: s = "aacecaaa"
-Output: "aaacecaaa"
+| Input: s = "aacecaaa"
+| Output: "aaacecaaa"
+
 Example 2:
 
-Input: s = "abcd"
-Output: "dcbabcd"
+| Input: s = "abcd"
+| Output: "dcbabcd"
 
 
 Constraints:
 
-0 <= s.length <= 5 * 104
-s consists of lowercase English letters only.
+- `0 <= s.length <= 5 * 10^4`
+- `s` consists of lowercase English letters only.
 """
 
 
