c9c0e2f0cbd4f24e618b76bad374a97f933b25a2
jake-olvin <jake@olvin.com>
2024-10-04 16:51:39 +0100

    refactor to add type hint

--- tests/test_problem_214.py
+++ tests/test_problem_214.py
@@ -2,55 +2,55 @@ from leetcode.problem_214 import Solution
 
 
 class TestSolution:
-    def setup_method(self):
+    def setup_method(self) -> None:
         self.func = Solution().shortestPalindrome
 
-    def test_case_1(self):
+    def test_case_1(self) -> None:
         res = self.func("aacecaaa")
         ans = "aaacecaaa"
         assert res == ans
 
-    def test_case_2(self):
+    def test_case_2(self) -> None:
         res = self.func("abcd")
         ans = "dcbabcd"
         assert res == ans
 
-    def test_case_3(self):
+    def test_case_3(self) -> None:
         res = self.func("a")
         ans = "a"
         assert res == ans
 
-    def test_case_4(self):
+    def test_case_4(self) -> None:
         res = self.func("racecar")
         ans = "racecar"
         assert res == ans
 
-    def test_case_5(self):
+    def test_case_5(self) -> None:
         res = self.func("baa")
         ans = "aabaa"
         assert res == ans
 
-    def test_case_6(self):
+    def test_case_6(self) -> None:
         res = self.func("abc")
         ans = "cbabc"
         assert res == ans
 
-    def test_case_7(self):
+    def test_case_7(self) -> None:
         res = self.func("fgfhf")
         ans = "fhfgfhf"
         assert res == ans
 
-    def test_case_8(self):
+    def test_case_8(self) -> None:
         res = self.func("zoog")
         ans = "goozoog"
         assert res == ans
 
-    def test_case_9(self):
+    def test_case_9(self) -> None:
         res = self.func("rqiqreur")
         ans = "ruerqiqreur"
         assert res == ans
 
-    def test_case_10(self):
+    def test_case_10(self) -> None:
         res = self.func("qwerty")
         ans = "ytrewqwerty"
         assert res == ans
--- tests/test_problem_27.py
+++ tests/test_problem_27.py
@@ -2,35 +2,35 @@ from leetcode.problem_27 import Solution
 
 
 class TestSolution:
-    def setup_method(self):
+    def setup_method(self) -> None:
         self.func = Solution().removeElement
 
-    def test_case_1(self):
+    def test_case_1(self) -> None:
         res = self.func(nums=[3, 2, 2, 3], val=3)
         ans = 2
         assert res == ans
 
-    def test_case_2(self):
+    def test_case_2(self) -> None:
         res = self.func(nums=[0, 1, 2, 2, 3, 0, 4, 2], val=0)
         ans = 6
         assert res == ans
 
-    def test_case_3(self):
+    def test_case_3(self) -> None:
         res = self.func(nums=[0, 1, 2, 2, 3, 0, 4, 2], val=1)
         ans = 7
         assert res == ans
 
-    def test_case_4(self):
+    def test_case_4(self) -> None:
         res = self.func(nums=[0, 1, 2, 2, 3, 0, 4, 2], val=2)
         ans = 5
         assert res == ans
 
-    def test_case_5(self):
+    def test_case_5(self) -> None:
         res = self.func(nums=[0, 1, 2, 2, 3, 0, 4, 2], val=3)
         ans = 7
         assert res == ans
 
-    def test_case_6(self):
+    def test_case_6(self) -> None:
         res = self.func(nums=[0, 1, 2, 2, 3, 0, 4, 2], val=4)
         ans = 7
         assert res == ans
